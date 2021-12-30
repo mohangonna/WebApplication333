bb89d900051ac1714389f45bdffc353568f21afe
Mohan Krishna <mohan.gonna@gmail.com>
2021-12-30 11:10:17 +0530

    Commit

---
+++ WebApplication333/Controllers/EMPController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication333.Controllers
+{
+    public class EMPController : Controller
+    {
+        // GET: EMP
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
